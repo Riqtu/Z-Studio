783205e695019f5aab321f44d333bfcb974ed727
Riqtu <xxx-zet@mail.ru>
2018-03-26 12:59:45 +0300

    new concept

--- js/main.js
+++ js/main.js
@@ -1,24 +1,15 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         var bo = $(this).scrollTop();
-        var a = $(".welcome").css('opacity')
-        var b = $(".about").css('opacity')
-    if ( bo >= 200 && a==1 && b==0) {
-        $(".welcome").animate({'opacity':'0'},500)
-        $(".welcome__flat").animate({'top':'-100px'},1000)
-        $(".welcome__hello").animate({'top':'-90px'},1050)
-        $(".welcome__text").animate({'top':'-90px'},1100)
-        $(".about").animate({'opacity':'1'},700)
-        $(".bar__welcome").animate({'opacity':'0'},500)
+    if ( bo >= 400) {
+        $(".bar__welcome").animate({'opacity':'0'},10)
+        $(".bar__about").animate({'opacity':'1'},10)
     };
-    if ( bo < 200 && a==0 && b==1) {
-        $(".welcome").animate({'opacity':'1'},500)
-        $(".welcome__flat").animate({'top':'330px'},400)
-        $(".welcome__hello").animate({'top':'430px'},400)
-        $(".welcome__text").animate({'top':'500px'},400)
-        $(".about").animate({'opacity':'0'},700)
-        $(".bar__welcome").animate({'opacity':'1'},500)
+    if ( bo < 400) {
+        $(".bar__welcome").animate({'opacity':'1'},10)
+        $(".bar__about").animate({'opacity':'0'},10)
     };
+
     })
 })
 
@@ -42,7 +33,7 @@ function menuoff(){
     }, 700);
     $(".menu img").animate({'opacity':'1'},300)
     setTimeout(() => {
-        $(".menu img").css("display","block");
+        $(".menu img").css("display","grid");
     }, 1);
     $(".mleft").animate({'top':'-100%'},500)
     $(".mcenter").animate({'top':'100%'},500)
