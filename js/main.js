cc2e927c6e011bf6932f85f83fbfeae57d1f6fc0
Riqtu <xxx-zet@mail.ru>
2018-03-26 22:44:57 +0300

    adding other cool styles

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 $(document).ready(function(){
     $(window).scroll(function(){
         var bo = $(this).scrollTop();
-    if ( bo >= 400) {
-        $(".bar__welcome").animate({'opacity':'0'},10)
-        $(".bar__about").animate({'opacity':'1'},10)
+    if ( bo >= 800) {
+        $(".bar__welcome").css('opacity','0')
+        $(".bar__about").css('opacity','0')
+    };
+    if ( bo >= 400 && bo<800) {
+        $(".bar__welcome").css('opacity','0')
+        $(".bar__about").css('opacity','1')
     };
     if ( bo < 400) {
-        $(".bar__welcome").animate({'opacity':'1'},10)
-        $(".bar__about").animate({'opacity':'0'},10)
+        $(".bar__welcome").css('opacity','1')
+        $(".bar__about").css('opacity','0')
     };
-
     })
 })
-
+function goToAbout(){
+}
 function menuon(){
     $(".menu img").animate({'opacity':'0'},300)
     setTimeout(() => {
@@ -21,6 +25,9 @@ function menuon(){
     $(".novigation").animate({'opacity':'1'},700)
     setTimeout(() => {
         $(".novigation").css("display","grid");
+        $(".mleft").css("display","block");
+        $(".mcenter").css("display","block");
+        $(".mright").css("display","block");
     }, 1);
     $(".mleft").animate({'top':'0%'},500)
     $(".mcenter").animate({'top':'0%'},500)
@@ -30,6 +37,9 @@ function menuoff(){
     $(".novigation").animate({'opacity':'0'},300)
     setTimeout(() => {
         $(".novigation").css("display","none");
+        $(".mleft").css("display","none");
+        $(".mcenter").css("display","none");
+        $(".mright").css("display","none");
     }, 700);
     $(".menu img").animate({'opacity':'1'},300)
     setTimeout(() => {
