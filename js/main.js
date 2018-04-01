79c478b99b57105b768e21b83d148f68f90f4a44
Riqtu <xxx-zet@mail.ru>
2018-04-01 21:04:16 +0300

    add projects logistic

--- js/main.js
+++ js/main.js
@@ -51,3 +51,84 @@ function menuoff(){
     $(".mcenter").animate({'top':'100%'},500)
     $(".mright").animate({'top':'-100%'},500)
 }
+
+function logo(){
+    $(".projects__logo").css("display","block");
+    setTimeout(() => {
+        $(".projects__logo").animate({'opacity':'1'},500)
+    }, 1);
+}
+function logoExit(){
+    $(".projects__logo").animate({'opacity':'0'},500)
+    setTimeout(() => {
+        $(".projects__logo").css("display","none");
+    }, 501);
+}
+
+function logoItem1On(){
+    $(".logo__fullsize").css("background-image","url('img/item1.png')");
+}
+function logoItem2On(){
+    $(".logo__fullsize").css("background-image","url('img/item2.png')");
+}
+function logoItem3On(){
+    $(".logo__fullsize").css("background-image","url('img/item3.png')");
+}
+function logoItem4On(){
+    $(".logo__fullsize").css("background-image","url('img/item4.png')");
+}
+
+
+
+function youtube(){
+    $(".projects__youtube").css("display","block");
+    setTimeout(() => {
+        $(".projects__youtube").animate({'opacity':'1'},500)
+    }, 1);
+}
+function youtubeExit(){
+    $(".projects__youtube").animate({'opacity':'0'},500)
+    setTimeout(() => {
+        $(".projects__youtube").css("display","none");
+    }, 501);
+}
+
+function youtubeItem1On(){
+    $(".youtube__fullsize").css("background-image","url('img/item1.png')");
+}
+function youtubeItem2On(){
+    $(".youtube__fullsize").css("background-image","url('img/item2.png')");
+}
+function youtubeItem3On(){
+    $(".youtube__fullsize").css("background-image","url('img/item3.png')");
+}
+function youtubeItem4On(){
+    $(".logo__fullsize").css("background-image","url('img/item4.png')");
+}
+
+
+function web(){
+    $(".projects__web").css("display","block");
+    setTimeout(() => {
+        $(".projects__web").animate({'opacity':'1'},500)
+    }, 1);
+}
+function webExit(){
+    $(".projects__web").animate({'opacity':'0'},500)
+    setTimeout(() => {
+        $(".projects__web").css("display","none");
+    }, 501);
+}
+
+function webItem1On(){
+    $(".web__fullsize").css("background-image","url('img/item1.png')");
+}
+function webItem2On(){
+    $(".web__fullsize").css("background-image","url('img/item2.png')");
+}
+function webItem3On(){
+    $(".web__fullsize").css("background-image","url('img/item3.png')");
+}
+function webItem4On(){
+    $(".logo__fullsize").css("background-image","url('img/item4.png')");
+}
